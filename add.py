530f1d9581812daca85da5bf3d3a14d4987e3adc
user <user@fisat>
2018-04-18 04:57:30 -0400

    create 4 numbers to ht e function

--- add.py
+++ add.py
@@ -4,3 +4,22 @@ def add2(n1, n2):
 
 		print add2(3, 4)
 	
+
+
+
+			
+def add3(n1, n2, n3):
+			s = n1 + n2 + n3
+			return s
+
+		print add3(3, 4, 5)
+	
+
+			
+def add4(n1, n2, n3, n4):
+			s = n1 + n2 + n3 + n4
+			return s
+
+		print add4(3, 4, 5, 6)
+	
+
